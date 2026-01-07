3f00db39bc76aa319295097fb0cfa531046d56b7
Nupur1807 <noopur414@gmail.com>
2026-01-07 19:48:46 +0530

    updated code

--- exception/custom_exception.py
+++ exception/custom_exception.py
@@ -52,12 +52,12 @@ class DocumentPortalException(Exception):
         return f"DocumentPortalException(file={self.file_name!r}, line={self.lineno}, message={self.error_message!r})"
 
 
-# if __name__ == "__main__":
-#     # Demo-1: generic exception -> wrap
-#     try:
-#         a = 1 / 0
-#     except Exception as e:
-#         raise DocumentPortalException("Division failed", e) from e
+if __name__ == "__main__":
+    # Demo-1: generic exception -> wrap
+    try:
+        a = 1 / 0
+    except Exception as e:
+        raise DocumentPortalException("Division failed", e) from e
 
     # Demo-2: still supports sys (old pattern)
     # try:
